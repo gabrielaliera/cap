ab042acc1185b5018f0183ea461490ddc1ec18c3
gabrielaliera <gliera2990@gmail.com>
2023-03-13 10:18:21 -0700

    Add text box map

--- src/Components/APIform.jsx
+++ src/Components/APIform.jsx
@@ -1,13 +1,29 @@
 
 
 
-const APIForm = () => {
+const APIForm = ({inputs, handleChange, onSubmit}) => {
 
     return (
       <div>
         <h2> Select Your Image Attributes: </h2>
         <form className="form-container">
-          
+            {inputs &&
+             Object.entries(inputs).map(([category, value], index) => (
+                <li className="form" key={index}>
+                    <h2>{category}</h2>
+                    <input
+                        type="text"
+                        name={category}
+                        value={value}
+                        placeholder="Input this attribute..."
+                        onChange={handleChange}
+                        className="textbox"
+                    />
+                    <br></br>
+                    <br></br>
+                    <p> {inputsInfo[index]}</p>
+                </li>
+            ))}
         </form>
       </div>
     );
