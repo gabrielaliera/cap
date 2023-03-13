c22afa172b5bfa4bf249d2b040be9bb310f80950
gabrielaliera <gliera2990@gmail.com>
2023-03-13 10:14:43 -0700

    Add access key and  default inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import './App.css'
+import './App.css';
+import React from 'react';
+import { useState } from 'react';
+
+const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
+
 
 function App() {
   
-  return (
-    
+  const [inputs, setInputs] = useState({
+    url: "",
+    format: "",
+    no_ads: "",
+    no_cookie_banners: "",
+    width: "",
+    height: "",
+  });
+  
+  return (  
+    <div>Hi</div>
   )
 }
 
