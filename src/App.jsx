737a5df5900a6e4d1154e3ef961b14889de42c28
gabrielaliera <gliera2990@gmail.com>
2023-03-13 10:33:23 -0700

    Create makeQuery function

--- src/App.jsx
+++ src/App.jsx
@@ -34,11 +34,22 @@ function App() {
           inputs[key] = defaultValues[key]
         }
       }
+      makeQuery();
     }
 
   }
 
+  const makeQuery = () => {
+    let wait_until = "network_idle";
+    let response_type = "json";
+    let fail_on_status = "400%2C404%2C500-511";
+    let url_starter = "https://";
+    let fullURL = url_starter + inputs.url;
   
+    let query = `https://api.apiflash.com/v1/urltoimage?access_key=${ACCESS_KEY}&url=${fullURL}&format=${inputs.format}&width=${inputs.width}&height=${inputs.height}&no_cookie_banners=${inputs.no_cookie_banners}&no_ads=${inputs.no_ads}&wait_until=${wait_until}&response_type=${response_type}&fail_on_status=${fail_on_status}`;
+  }
+
+
 
   return (  
     <div className="whole-page">
