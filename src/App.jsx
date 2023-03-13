82d509d40be4f19edebda260445b60a6e45c78a8
gabrielaliera <gliera2990@gmail.com>
2023-03-13 10:30:36 -0700

    Create submitForm function

--- src/App.jsx
+++ src/App.jsx
@@ -18,9 +18,28 @@ function App() {
   });
   
   const submitForm = () =>{
+    let defaultValues = {
+      format: "jpeg",
+      no_ads: "true",
+      no_cookie_banners: "true",
+      width: "1920",
+      height: "1080",
+    };
+
+    if (inputs.url == "" || inputs.url == " ") {
+      alert("You forgot to submit an url!");
+    } else {
+      for (const [key, value] of Object.entries(inputs)) {
+        if (value == ""){
+          inputs[key] = defaultValues[key]
+        }
+      }
+    }
 
   }
+
   
+
   return (  
     <div className="whole-page">
       <h1>Build Your Own Screenshot! 📸</h1>
